7e345ae2864ca3344ce7a53c335c97e304443a73
Roberto Trinidad <Roberto Trinidad@DESKTOP-DPC9LH3>
2024-02-13 21:16:29 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("asigna un valor para saber si es par o impar");
+int num = int.Parse(Console.ReadLine());
+
+
+if ((num % 2) == 0)
+{
+    Console.WriteLine("Es par ");
+    Console.ReadKey();
+}
+else
+    Console.WriteLine("Es impar ");
+
+Console.ReadKey();
